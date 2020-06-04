f8f19384b189f4bdafab2ea1fdfbcd5a4940ad53
Amir <amiromayer@gmail.com>
2020-06-04 23:11:07 +0500

    Add deep links support

--- src/navigation/RootNavigator.js
+++ src/navigation/RootNavigator.js
@@ -1,21 +1,30 @@
 import React, { useState } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { enableScreens } from 'react-native-screens'
-
+import { Text } from 'react-native'
 import { createStackNavigator } from '@react-navigation/stack'
 
 import AppNavigator from './AppNavigator'
 import SignInScreen from '../screens/SignInScreen'
 
+import { Linking } from 'expo'
+
 enableScreens()
 
 const Stack = createStackNavigator()
 
+const prefix = Linking.makeUrl('/')
+console.log('prefix: ', prefix)
+
 const RootNavigator = () => {
   const [isSignedIn, setIsSignedIn] = useState(false)
 
+  const linking = {
+    prefixes: [prefix]
+  }
+
   return (
-    <NavigationContainer>
+    <NavigationContainer linking={linking} fallback={<Text>Loading...</Text>}>
       <Stack.Navigator
         initialRouteName='SignIn'
         screenOptions={{ headerShown: false, gestureEnabled: false }}
